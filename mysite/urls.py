f60d1f822ff59fe3e91993312949bbca435e905a
mhatanorks <masaki.hatano@rakus-partners.co.jp>
2023-11-09 20:01:28 +0900

    fixed urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
-    path("api/auth/", include("dj_rest_auth.urls")), # 追加
+    # path("api/auth/", include("dj_rest_auth.urls")), # 追加
     path("api/social/login/", include("accounts.urls")), # 追加
 ]
